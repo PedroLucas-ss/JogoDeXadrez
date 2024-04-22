d79c1a180c9995bd9baeb827fb4c0467bb00885d
Pedro <pedroluc1232@gmail.com>
2024-04-22 09:25:53 -0300

    criando os movimentos da peca rei

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Colors;
 
@@ -17,10 +18,63 @@ public class King extends ChessPiece {
     public String toString(){
         return "K";
     }
+    private boolean canMove(Position position){
 
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
     @Override
     public boolean[][] possivelMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+
+        Position p = new Position(0,0);
+        //cima
+        p.setValues(position.getRow()-1, position.getColumn());
+        if(getBoard().positionExists(p)&& canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //baixo
+        p.setValues(position.getRow()+1, position.getColumn());
+        if(getBoard().positionExists(p)&& canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //esq
+        p.setValues(position.getRow(), position.getColumn()-1);
+        if(getBoard().positionExists(p)&& canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //dir
+        p.setValues(position.getRow(), position.getColumn()+1);
+        if(getBoard().positionExists(p)&& canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //esq-cima
+        p.setValues(position.getRow()-1, position.getColumn()-1);
+        if(getBoard().positionExists(p)&& canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //dir-cima
+        p.setValues(position.getRow()-1, position.getColumn()+1);
+        if(getBoard().positionExists(p)&& canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //esq-baixo
+        p.setValues(position.getRow()+1, position.getColumn()-1);
+        if(getBoard().positionExists(p)&& canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //esq-baixo
+        p.setValues(position.getRow()+1, position.getColumn()+1);
+        if(getBoard().positionExists(p)&& canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+
+
+
         return mat;
+
+
     }
 }
